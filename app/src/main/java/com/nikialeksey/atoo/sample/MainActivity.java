c749eeea132fc76fcec3d0a2ef3756679c0b3e4f
Alexey Nikitin <nikialeksey@gmail.com>
2017-08-23 14:00:56 +0700

    Added first todo

--- app/src/main/java/com/nikialeksey/atoo/sample/MainActivity.java
+++ app/src/main/java/com/nikialeksey/atoo/sample/MainActivity.java
@@ -3,5 +3,5 @@ package com.nikialeksey.atoo.sample;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
-
+    // @todo #1:30m Add glsurfaceview for fullscreen with active status bar
 }
