4a5502c0ae539a1e3afe221c4cf8017fa68f9fcf
Alexey Nikitin <nikialeksey@gmail.com>
2017-08-25 12:54:55 +0700

    Trying glsurfaceview

--- app/src/main/java/com/nikialeksey/atoo/sample/MainActivity.java
+++ app/src/main/java/com/nikialeksey/atoo/sample/MainActivity.java
@@ -1,7 +1,43 @@
 package com.nikialeksey.atoo.sample;
 
 import android.app.Activity;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends Activity {
     // @todo #1:30m Add glsurfaceview for fullscreen with active status bar
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        final GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(final GL10 gl10, final EGLConfig eglConfig) {
+                GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            }
+
+            @Override
+            public void onSurfaceChanged(final GL10 gl10, final int width, final int height) {
+                GLES20.glViewport(0, 0, width, height);
+            }
+
+            @Override
+            public void onDrawFrame(final GL10 gl10) {
+                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+            }
+        });
+        setContentView(view);
+    }
 }
